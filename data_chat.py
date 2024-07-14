ba7e6cdf6f68bae7875e9c87c4c50d3643ab7429
Jordan <64715176+onionsp@users.noreply.github.com>
2024-07-14 20:17:35 +1000

    update

--- data_chat.py
+++ data_chat.py
@@ -1,26 +1,30 @@
+import os
 import streamlit as st
 import pandas as pd
 from pandasai import SmartDataframe
-from pandasai.llm import OpenAI
+from pandasai.llm import GoogleGemini
 from pandasai.responses.response_parser import ResponseParser
 
+TOKEN = st.secrets["TOKEN"]
+
 def show_data_chat():
-    st.write("# Chat with Developer Survey Dataset")
+    st.write("# reChat with Developer Survey Dataset")
 
     df = pd.read_csv("./data/survey_results_public.csv")
 
     with st.expander("🔎 Dataframe Preview"):
         st.write(df.tail(3))
 
-    query = st.text_area("🗣️ Chat with Dataframe")
+    query = st.text_area("🗣️ Chat with Dataframe using Gemini")
     container = st.container()
 
     if query:
-        llm = OpenAI(api_token=os.environ["sk-proj-keRFWakhymVhcc2aNxF0T3BlbkFJvA0ujStFB7yM6gHwwLP3"])
+        llm = GoogleGemini(api_key=TOKEN)
         query_engine = SmartDataframe(
             df,
             config={
                 "llm": llm,
+            
             },
         ) 
         answer = query_engine.chat(query)
